32c3aee15dc8265781be5f713e84c3a8c559b15b
Seo-JS0823 <seong0093571@naver.com>
2025-09-12 00:33:16 +0900

    version 0.0.6

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,23 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const logo = document.querySelector('[id=logo]');
-	logo.addEventListener('click', () => {
-		let nav = document.querySelector('[id=account-nav]');
-		let a = document.querySelectorAll('[class=item-a]');
-		const w = getComputedStyle(nav).width;
-		
-		if(w == '103px') {
-			a.forEach( (el) => {
-				setTimeout(() => {
-					el.style.display = 'block';					
-				}, 300);
-			});
-			nav.style.width = '300px';
+	logo.addEventListener('click', (e) => {
+		if(window.innerWidth > 900) {
+			let nav = document.querySelector('[id=account-nav]');
+			let a = document.querySelectorAll('[class=item-a]');
+			const w = getComputedStyle(nav).width;
+			
+			if(w == '60px') {
+				e.preventDefault();
+				return false;
+			}
+			
+			if(w == '103px') {
+				a.forEach( (el) => {
+					setTimeout(() => {
+						el.style.display = '';					
+					}, 300);
+				});
+				nav.style.width = '300px';
+			} else {
+				a.forEach( (el) => {
+					el.style.display = 'none';
+				});
+				nav.style.width = '103px';
+			}
+		}
+	});
+	window.addEventListener('resize', () => {
+		let nav = document.getElementById('account-nav');
+		let a = document.querySelectorAll('.item-a');
+		if (window.innerWidth <= 900) {
+			nav.style.width = '';
 		} else {
 			a.forEach( (el) => {
-				el.style.display = 'none';
+				el.style.display = '';
 			});
-			nav.style.width = '103px';
 		}
-		
 	});
 });
